5da6385027a48ddc2c49b3a046b6b64bc6656886
Leandro Rodrigues <leandrox7_@hotmail.com>
2020-09-05 20:40:22 -0300

    Adicionado novo gatilho e procedure para conta Prime

--- procedimento.sql
+++ procedimento.sql
@@ -1,4 +1,5 @@
 
+SELECT* FROM prime;
 
 -- criação de um canal novo
 	INSERT INTO CanalStreamer VALUES('Seja bem vindo a Twitch',FALSE,'Twitch','dfADFE#43DFGSI3',4);
@@ -15,6 +16,7 @@ CREATE PROCEDURE seguir_canal_oficial (IN cod_streamer int)
 	BEGIN
 		INSERT INTO Seguir VALUES(4, cod_streamer);
         -- primeiro digito é o canal, o segundo é o seguidor
+        
 	END $$
 DELIMITER 
 
@@ -46,3 +48,44 @@ DELIMITER
 INSERT INTO CanalStreamer VALUES('Seja bem vindo a LeandroLândia',FALSE,'Leandro','dfAawI3',5);
 
 select* from seguir
+
+
+
+/* Segundo Gatilho */
+
+-- --------------------------------------------------------
+
+/*
+GATILHO
+	É DISPARADO SEMPRE QUE UMA CONTA PRIME É ADICIONADO NA PLATAFORMA.
+    O QUE FAZ? CHAMA A PROCEDURE QUE ADICIONA UMA RECOMPENSA
+    
+*/
+DELIMITER $$
+		CREATE TRIGGER bonus_prime AFTER INSERT
+		ON prime
+		FOR EACH ROW
+		BEGIN
+		CALL recompensa_prime(NEW.cod_amazon);
+	END $$
+DELIMITER 
+
+-- PROCEDIMENTO
+	/* 
+		O QUE FAZ: ADICIONA AO NOVO PRIME O ÚLTIMO ITEM CADASTRADO
+	*/
+    
+DELIMITER $$
+CREATE PROCEDURE recompensa_prime (IN cod_amazon INT)
+	BEGIN
+		INSERT INTO Recompensa VALUES(
+          (SELECT  cod_item FROM items ORDER BY cod_item DESC LIMIT 1),cod_amazon,TRUE);
+
+	END $$
+DELIMITER 
+
+/* COMO CHAMAR O PROCEDIMENTO?
+	CALL recompensa_prime(cod_prime);
+*/
+
+SELECT* FROM prime
